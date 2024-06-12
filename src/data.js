f1f4cca2ae294abf4c84f089531772bfe6146a42
gitName <a@b.c>
2024-06-12 14:50:23 -0400

    separate json data from code

---
+++ src/data.js
@@ -0,0 +1,178 @@
+// src/data.js
+const data = [
+    {
+      "categoria": "DIETAS ESTANDAR",
+      "tipo": "POLIMERICAS",
+      "variedad": "NORMOPROTEICAS",
+      "nombre_comercial": "FRESUBIN ORIGINAL",
+      "presentacion": "500 ml",
+      "kcal_por_unidad": 500,
+      "kcal_por_ml": 1,
+      "mosm_por_l": 220,
+      "kcal_no_prot_por_g_N": 143,
+      "proteinas": {
+        "energia_porcentaje": 15,
+        "g_por_unidad_por_1000_kcal": "19/38",
+        "fuente": "Caseína, Soja"
+      },
+      "h_carbono": {
+        "energia_porcentaje": 55,
+        "g_por_unidad_por_1000_kcal": "69/138",
+        "fuente": "Mdx 73%, Sacarosa 27%"
+      },
+      "lipidos": {
+        "energia_porcentaje": 30,
+        "g_por_unidad_por_1000_kcal": "17/34",
+        "fuente": "Girasol, Pescado",
+        "AGS_AGM_AGP": "MCT 38%"
+      },
+      "fibra": {
+        "g_por_unidad": 0,
+        "tipo": "Sin fibra"
+      },
+      "na_mg_por_unidad": 375,
+      "k_mg_por_unidad": 625,
+      "observaciones": "Lactosa ≤ 0,05 g, No gluten",
+      "sabor": "Vainilla"
+    },
+    {
+      "categoria": "DIETAS ESTANDAR",
+      "tipo": "POLIMERICAS",
+      "variedad": "NORMOPROTEICAS",
+      "nombre_comercial": "OSMOLITE HN RTH",
+      "presentacion": "500 ml",
+      "kcal_por_unidad": 500,
+      "kcal_por_ml": 1,
+      "mosm_por_l": 244,
+      "kcal_no_prot_por_g_N": 135,
+      "proteinas": {
+        "energia_porcentaje": 16,
+        "g_por_unidad_por_1000_kcal": "20/40",
+        "fuente": "Caseinatos, Soja"
+      },
+      "h_carbono": {
+        "energia_porcentaje": 54,
+        "g_por_unidad_por_1000_kcal": "68/135.6",
+        "fuente": "Mdx 100%"
+      },
+      "lipidos": {
+        "energia_porcentaje": 30,
+        "g_por_unidad_por_1000_kcal": "17/34",
+        "fuente": "Girasol rico oleico 48%, Cánola 29%, MCT 19%"
+      },
+      "fibra": {
+        "g_por_unidad": 0,
+        "tipo": "Sin fibra"
+      },
+      "na_mg_por_unidad": 440,
+      "k_mg_por_unidad": 740,
+      "observaciones": "No lactosa, No gluten (No apto galactosémicos)",
+      "sabor": "Neutro"
+    },
+    {
+      "categoria": "DIETAS ESTANDAR",
+      "tipo": "POLIMERICAS",
+      "variedad": "NORMOPROTEICAS",
+      "nombre_comercial": "FRESUBIN ORIGINAL FIBRA",
+      "presentacion": "500 ml",
+      "kcal_por_unidad": 500,
+      "kcal_por_ml": 1,
+      "mosm_por_l": 250,
+      "kcal_no_prot_por_g_N": 143,
+      "proteinas": {
+        "energia_porcentaje": 15,
+        "g_por_unidad_por_1000_kcal": "19/38",
+        "fuente": "Caseína, Soja"
+      },
+      "h_carbono": {
+        "energia_porcentaje": 55,
+        "g_por_unidad_por_1000_kcal": "69/138",
+        "fuente": "Mdx 100%"
+      },
+      "lipidos": {
+        "energia_porcentaje": 30,
+        "g_por_unidad_por_1000_kcal": "17/34",
+        "fuente": "Girasol 60%, MCT 40%"
+      },
+      "fibra": {
+        "g_por_unidad": 10,
+        "tipo": "61% Soluble"
+      },
+      "na_mg_por_unidad": 665,
+      "k_mg_por_unidad": 775,
+      "observaciones": "Lactosa ≤ 0,05 g, No gluten",
+      "sabor": "Neutro"
+    },
+    {
+      "categoria": "DIETAS ESTANDAR",
+      "tipo": "POLIMERICAS",
+      "variedad": "HIPERPROTEICAS",
+      "nombre_comercial": "FRESUBIN ENERGY DRINK",
+      "presentacion": "200 ml",
+      "kcal_por_unidad": 300,
+      "kcal_por_ml": 1.5,
+      "mosm_por_l": 355,
+      "kcal_no_prot_por_g_N": 142,
+      "proteinas": {
+        "energia_porcentaje": 15,
+        "g_por_unidad_por_1000_kcal": "11.2/37.3",
+        "fuente": "Lácteas"
+      },
+      "h_carbono": {
+        "energia_porcentaje": 50,
+        "g_por_unidad_por_1000_kcal": "37.6/125.3",
+        "fuente": "Mdx, Sacarosa"
+      },
+      "lipidos": {
+        "energia_porcentaje": 35,
+        "g_por_unidad_por_1000_kcal": "11.6/38.6",
+        "fuente": "Vegetal, MCT"
+      },
+      "fibra": {
+        "g_por_unidad": 0,
+        "tipo": "Sin fibra"
+      },
+      "na_mg_por_unidad": "160/170*",
+      "k_mg_por_unidad": "270/280*",
+      "observaciones": "Lactosa ≤ 0,54 g, No gluten",
+      "sabor": "Fresa, Vainilla, Chocolate*"
+    },
+    {
+      "categoria": "DIETAS ESTANDAR",
+      "tipo": "POLIMERICAS",
+      "variedad": "HIPERPROTEICAS",
+      "nombre_comercial": "FORTIMEL EXTRA",
+      "presentacion": "200 ml",
+      "kcal_por_unidad": 405,
+      "kcal_por_ml": 2.0,
+      "mosm_por_l": 730,
+      "kcal_no_prot_por_g_N": 76,
+      "proteinas": {
+        "energia_porcentaje": 20,
+        "g_por_unidad_por_1000_kcal": "20/100",
+        "fuente": "Caseína 86%, Prot. séricas"
+      },
+      "h_carbono": {
+        "energia_porcentaje": 41,
+        "g_por_unidad_por_1000_kcal": "41.2/206",
+        "fuente": "Mdx 37%, Sacarosa 29%, Lactosa 6%"
+      },
+      "lipidos": {
+        "energia_porcentaje": 39,
+        "g_por_unidad_por_1000_kcal": "17.2/86",
+        "fuente": "Vegetales 97%, Lácteos 3%",
+        "AGS_AGM_AGP": "0,88/5.2/2.5"
+      },
+      "fibra": {
+        "g_por_unidad": 0,
+        "tipo": "Sin fibra"
+      },
+      "na_mg_por_unidad": 170,
+      "k_mg_por_unidad": 344,
+      "observaciones": "No lactosa, No gluten (No apto galactosémicos)",
+      "sabor": "Neutro"
+    }
+  ];
+  
+  export default data;
+  
